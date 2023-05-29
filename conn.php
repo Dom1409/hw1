86d2f77df53a84991b4841c107d65550593976de
Dom1409 <128229369+Dom1409@users.noreply.github.com>
2023-05-29 06:54:16 +0200

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,4 @@
+<?php 
+    $conn=mysqli_connect("localhost","root","","sitohw1") or die("Errore : ".mysqli_connect_error());
+
+?>
---
+++ database.sql
@@ -0,0 +1,23 @@
+use sitohw1;
+
+create table user(
+    id integer primary key auto_increment,
+	nome varchar(30) not null,
+	cognome varchar(30) not null,
+    username varchar(30) not null,
+    password varchar(30) not null
+)Engine = InnoDB;
+
+drop table user;
+
+
+create table lista_desideri(
+	id_user integer not null,
+    content json,
+    foreign key (id_user) references user(id) on update cascade
+)Engine = InnoDB;
+
+drop table lista_desideri;
+
+
+SELECT* FROM lista_desideri WHERE content->>'$.nome' is not null;
